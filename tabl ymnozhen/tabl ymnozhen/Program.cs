eb6b1c2f5db1affd40978f64add065278f5a6629
Kirill <91422323+k1dobu3@users.noreply.github.com>
2021-10-31 18:18:44 +0300

    Add files via upload

---
+++ tabl ymnozhen/tabl ymnozhen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace tabl_ymnozhen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите Start для вывода таблицы умножения!");
+            string xx = Console.ReadLine();
+            string zz = "Start";
+            int sum;
+            if (xx == zz)
+            {
+                for (int i = 2; i <= 6; i++)
+                {
+                    for (int e = 1; e <= 10; e++)
+                    {
+                        sum = i * e;
+                        Console.WriteLine($"{i} x {e} = {sum}");
+                    }
+                }
+            }
+            else  Console.WriteLine("Подумайте над своим поведением и вернитесь позже :3");
+
+
+
+
+
+
+
+
+
+
+
+            /* if (xx == zz)
+            {
+                while (a != 10)
+                    {
+                        a += 1;
+                        sum = q * a;
+                        Console.WriteLine($"{q} x {a} = {sum}");
+                        
+                    }
+                
+            } */
+        }
+    }
+}
